1525dce9c373c39902adfeaa13715c8377a98d68
Kevin Deggelman <kdeggelman@gmail.com>
2019-06-07 09:35:44 -0700

    Remove unused import

--- src/commands.ts
+++ src/commands.ts
@@ -8,7 +8,6 @@ import * as pify from 'pify';
 import * as vscode from 'vscode';
 import Config from './config';
 import Utils from './utils';
-import { pathToFileURL } from 'url';
 
 /* COMMANDS */
 
